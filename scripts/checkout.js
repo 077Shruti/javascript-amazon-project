4b53b40d2e0be1cbe6f77cf190469f638b93843e
Shruti Chauhan <sc9411963207@gmail.com>
2025-01-23 11:23:05 +0530

    fix price formatting and radio selectors

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,5 +1,6 @@
 import { cart } from "../data/cart.js";
 import { products } from "../data/products.js";
+import { formateCurrency } from "./utils/money.js";
 let cartSummarryHTML='';
 cart.forEach((cartItem) => {
     const productId=cartItem.productId;
@@ -11,7 +12,7 @@ cart.forEach((cartItem) => {
 
     });
   
-    
+    console.log(matchingProduct);
     cartSummarryHTML+=`
     <div class="cart-item-container">
             <div class="delivery-date">
@@ -20,18 +21,18 @@ cart.forEach((cartItem) => {
 
             <div class="cart-item-details-grid">
               <img class="product-image"
-                src="images/products/athletic-cotton-socks-6-pairs.jpg">
+                src="${matchingProduct.image}">
 
               <div class="cart-item-details">
                 <div class="product-name">
-                  Black and Gray Athletic Cotton Socks - 6 Pairs
+                  ${matchingProduct.name}
                 </div>
                 <div class="product-price">
-                  $10.90
+                 ${formateCurrency(matchingProduct.priceCents)}
                 </div>
                 <div class="product-quantity">
                   <span>
-                    Quantity: <span class="quantity-label">2</span>
+                    Quantity: <span class="quantity-label">${cartItem.quantity}</span>
                   </span>
                   <span class="update-quantity-link link-primary">
                     Update
@@ -49,7 +50,7 @@ cart.forEach((cartItem) => {
                 <div class="delivery-option">
                   <input type="radio" checked
                     class="delivery-option-input"
-                    name="delivery-option-1">
+                    name="delivery-option-${matchingProduct.id}">
                   <div>
                     <div class="delivery-option-date">
                       Tuesday, June 21
@@ -62,7 +63,7 @@ cart.forEach((cartItem) => {
                 <div class="delivery-option">
                   <input type="radio"
                     class="delivery-option-input"
-                    name="delivery-option-1">
+                    name="delivery-option-${matchingProduct.id}">
                   <div>
                     <div class="delivery-option-date">
                       Wednesday, June 15
@@ -75,7 +76,7 @@ cart.forEach((cartItem) => {
                 <div class="delivery-option">
                   <input type="radio"
                     class="delivery-option-input"
-                    name="delivery-option-1">
+                    name="delivery-option-${matchingProduct.id}">
                   <div>
                     <div class="delivery-option-date">
                       Monday, June 13
@@ -92,4 +93,4 @@ cart.forEach((cartItem) => {
 
 });
 
- document.querySelector('.js-order-summary').innerHTML=cartSummarryHTML;
+ document.querySelector('.js-order-summary').innerHTML=cartSummarryHTML
